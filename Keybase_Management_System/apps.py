3cb388b90b13aa2245109cf6fa627be70a40266b
sharif ahmad <sharif@rapidev.ae>
2020-03-19 17:27:24 +0500

    added useful message to app configs

--- Keybase_Management_System/apps.py
+++ Keybase_Management_System/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class KeybaseManagementSystemConfig(AppConfig):
     name = 'Keybase_Management_System'
+
+    def ready(self):
+        print(f'{self.name} -> initialized')
