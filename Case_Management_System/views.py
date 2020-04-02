03b7f0f5642b1f3e7728368b3a4f1d9c0b5bb691
awais azam <aa@rapidev.ae>
2020-04-02 19:32:39 +0500

    convert dictionary to linked nodes implimented

--- Case_Management_System/views.py
+++ Case_Management_System/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse,HttpResponseRedirect
 from django.shortcuts import render,reverse
-
+import json
 
 # Create your views here.
-
+n_series_data = []
 
 data = {
     "quiz": {
@@ -21,33 +21,60 @@ data = {
                 "answer": "Huston Rocket"
             }
         },
-        "maths": {
-            "q1": {
-                "question": "5 + 7 = ?",
-                "options": [
-                    "10",
-                    "11",
-                    "12",
-                    "13"
-                ],
-                "answer": "12"
-            },
-            "q2": {
-                "question": "12 - 8 = ?",
-                "options": [
-                    "1",
-                    "2",
-                    "3",
-                    "4"
-                ],
-                "answer": "4"
-            }
-        }
+
     }
 }
 
+
+data1 = {
+    "fruit": "Apple",
+    "size": "Large",
+    "color": "Red"
+}
+
+
 class Explore_Data(View):
     def get(self,request,*args,**kwargs):
-        print(data)
-        return render(request,'Case_Management_System/explore_data.html',{'data':data})
+
+        global n_series_data
+        n_series_data = []
+        resp = convert_json_to_network_series(data)
+        print(resp)
+        return render(request,'Case_Management_System/explore_data.html',{'data':resp})
+
+
+
+def convert_json_to_network_series(data,n=1):
+
+    f = (n/100) * 100
+    size = 5-f
+    node = None
+    for k,v in data.items():
+        if(not isinstance(v,dict)):
+
+            if(not isinstance(v,list)):
+                node = {'name':k,'value':size,'children':[{'name':v,'value':size-1}]}
+            else:
+                temp_childs = []
+                for val in v :
+                    child = {'name':val,'value':size-1}
+                    temp_childs.append(child)
+
+                node = {'name': k, 'value': size, 'children': temp_childs}
+
+            n_series_data.append(node)
+
+        else:
+            node = {'name':k,'value':size-1,'linkWith':list(v.keys())}
+            n_series_data.append(node)
+            resp = convert_json_to_network_series(v,n+0.5)
+
+
+    #print(n_series_data)
+    return json.dumps(n_series_data)
+
+
+
+
+
 
