99980f4009e38404f73263fab392e0792e94041e
awais azam <aa@rapidev.ae>
2020-04-29 13:08:02 +0500

    intigrating ais apis and confirmed

--- Public_Data_Acquisition_Unit/ais_api_controller.py
+++ Public_Data_Acquisition_Unit/ais_api_controller.py
@@ -23,16 +23,18 @@ class Ais_Api_Controller(object):
                 #self.ess_add_instagram_target(target_category='post')
                 #self.ess_add_instagram_search_target('islamabad')
         except Exception as e:
+            print(e)
             logger.error('.......................................failed to connect to AIS..........................')
 
     def ais_connect(self):
-        login_url = 'login/'
-        response = requests.post(AIS_SERVER_BASE_URL+login_url, data = {'username':AIS_SERVER_USER,'password':AIS_SERVER_PASSWORD})
+        login_url = 'login'
+        response = requests.post(AIS_SERVER_BASE_URL+login_url, data={},json = {'username':AIS_SERVER_USER,'password':AIS_SERVER_PASSWORD})
         if(response.status_code == 200):
             global AIS_API_TOKEN
             global Header
-            AIS_API_TOKEN = response.content.decode('utf-8').split(':')[1].strip('"}')
-            Header = {'Content-Type': 'application/json','Authorization': 'Token {0}'.format(AIS_API_TOKEN)}
+            AIS_API_TOKEN = response.content.decode('utf-8').split(':')[1].replace('"','').replace('}','').replace("\n",'')
+            print(AIS_API_TOKEN)
+            Header = {'Content-Type': 'application/json','Authorization': 'Bearer {0}'.format(AIS_API_TOKEN)}
             #logger.info('.............................connected to AIS server sucessfully ......................')
             print('.............................connected to AIS server sucessfully ......................')
             return True
@@ -54,7 +56,7 @@ class Ais_Api_Controller(object):
         try:
             add_target_url = 'text_analytics'
             payload = {'text':input_text,'task':operation}
-            response = requests.post(AIS_SERVER_BASE_URL+add_target_url,headers=Header,data=payload)
+            response = requests.post(AIS_SERVER_BASE_URL+add_target_url,headers=Header,json=payload)
             print(response.json())
             return response.json()
 
