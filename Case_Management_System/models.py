30fe191a94dfd3f9c5f64074b4af2d6481cd4cfb
sharif ahmad <sharif@rapidev.ae>
2020-05-10 23:31:51 -0700

    initial commit of case management system

--- Case_Management_System/models.py
+++ Case_Management_System/models.py
@@ -1,3 +1,31 @@
-from django.db import models
-
+"""
+contains all models for Case Management System
+"""
+from mongoengine import disconnect, connect, Document
+from mongoengine import EmbeddedDocument, EmbeddedDocumentListField
+from mongoengine import PointField, StringField, URLField
 # Create your models here.
+
+disconnect('default')
+connect(db='OSINT_System')
+
+
+class LocationOfInterest(EmbeddedDocument):
+    """
+    Embedded Doc that contains the specifics of each location
+    pertinent to a case. There will be multiple of these locations.
+    Notes and important details of each location will be stored in
+    **`description`**
+    """
+    location = PointField()
+    address = StringField()
+    description = StringField()
+
+class CaseCMS(Document):
+    """
+    central model class which will contain further
+    small and compound documents as Embedded Document
+    """
+    # police case files, server location (url)
+    case_files_location = URLField()
+    locations_of_interest = EmbeddedDocumentListField(LocationOfInterest)
--- Case_Management_System/urls.py
+++ Case_Management_System/urls.py
@@ -5,7 +5,7 @@ app_name = 'Case_Management_System'
 urlpatterns = [
 
     #path('target_response/',views.Target_Response.as_view(),name='target_response'),
-    path('explore_data/',views.Explore_Data.as_view(),name='explore_data'),
+    path('explore_data/', views.Explore_Data.as_view(), name='explore_data'),
 
 
 ]
