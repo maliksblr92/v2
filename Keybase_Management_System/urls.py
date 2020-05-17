219687651282dfcd6ec7ef239ac9d373483043b2
awais azam <aa@rapidev.ae>
2020-05-18 03:21:55 +0500

    block url simulation implimented

--- Keybase_Management_System/urls.py
+++ Keybase_Management_System/urls.py
@@ -9,6 +9,9 @@ urlpatterns = [
     path('archive/', views.Keybase_Archive.as_view(), name='kms_archive'),
     path('edit/', views.Edit_Keybase.as_view(), name='kms_edit'),
     path('delprop/', views.DeleteKeybaseProperty.as_view(), name='kms_delprop'),
-    path('keybase_fetched_responses/<str:GTR_id>', views.Keybase_Fetched_Responses.as_view(), name='Keybase_Fetched_Responses')
+    path('keybase_fetched_responses/<str:GTR_id>', views.Keybase_Fetched_Responses.as_view(), name='Keybase_Fetched_Responses'),
+    path('block_url/', views.Block_URL.as_view(),name='block_url'),
+    path('block_keybase_url/<str:url>/', views.Block_URL.as_view(),name='block_keybase_url'),
+    path('delete_url/<str:url_id>/', views.Delete_URL.as_view(),name='delete_url'),
 
 ]
