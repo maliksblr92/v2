099ff3d7a0352495683ffdfd63860ed8c8098ee7
ratroot92 <zcomisb.007@gmail.com>
2020-06-05 15:24:13 +0500

    before update from bugfix

--- Data_Processing_Unit/views.py
+++ Data_Processing_Unit/views.py
@@ -837,7 +837,6 @@ class Ip_Tools(View):
             resp = ess.get_domains_ip_info(domain)
             print(resp)
             if(resp['code'] == 0 ):
-                print("dictionary is zero redirecting ")
                 messages.success(request, 'No Response for the given domain ip ')
                 return render(request,'Data_Processing_Unit/ip_tools.html',{'query_type':query_type,'response':resp})
             else:
@@ -847,7 +846,13 @@ class Ip_Tools(View):
         elif query_type == 'domains_info':
             domain = request.POST['domain']
             resp = ess.get_domains_info(domain)
-
+            print("#####################################################################################")
+            print(resp)
+            if(resp['code'] == 0 ):
+                messages.success(request, 'No Response for the given domain ip ')
+                return render(request,'Data_Processing_Unit/ip_tools.html',{'query_type':query_type,'response':resp})
+            else:
+                return render(request,'Data_Processing_Unit/ip_tools.html',{'query_type':query_type,'response':resp})
 
 
         return render(request,'Data_Processing_Unit/ip_tools.html',{'query_type':query_type,'response':resp})
